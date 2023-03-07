518d085e6a3d0c5251d1ddcd690b4054f2db3dfc
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-07 23:38:17 +0200

    UPD features @ MainNavbar.tsx with responsive design

--- src/components/MainNavbar/MainNavbar.tsx
+++ src/components/MainNavbar/MainNavbar.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from "react";
+
 import navbarDataItemsLinks from "./navbar-data";
 
 import { Logo } from "../../data/photos";
 
 const MainNavbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleScrolling = () => {
+        if (window.scrollY > 180) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScrolling);
+
+        return () => {
+            window.removeEventListener("scroll", handleScrolling);
+        };
+    }, [isScrolled]);
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
+        <nav className={`navbar navbar-expand-lg py-3 fixed-top top-0 left-0 transitions-3 ${isScrolled ? "bg-white shadow-sm" : ""}`}>
             <div className="container">
-                <a href="#home">
+                <a href="#home" className={`${isScrolled ? "navbar-logo-invert" : ""}`}>
                     <img src={Logo} alt="IT Host" />
                 </a>
 
@@ -21,7 +41,8 @@ const MainNavbar = () => {
                             navbarDataItemsLinks.map(({ id, text, url, hasButtonShape }) => (
                                 <li key={id} className="nav-item">
                                     <a href={url}
-                                        className={`${hasButtonShape ? "custom-link" : ""} mx-2 fw-semibold text-capitalize`}>
+                                        className={`nav-item-link block py-3 ${hasButtonShape ? "custom-link" : ""} mx-3 text-capitalize 
+                                        text-decoration-none ${isScrolled ? "text-black" : "text-white"}`}>
                                         {text}
                                     </a>
                                 </li>
