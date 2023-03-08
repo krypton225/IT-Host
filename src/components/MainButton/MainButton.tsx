f26fd5108753d3c736fce7f794cc5b9b3722a91a
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-08 20:33:38 +0200

    UPD cols @ HostSolution in Home page

--- src/components/MainButton/MainButton.tsx
+++ src/components/MainButton/MainButton.tsx
@@ -2,8 +2,8 @@ import IMainButton from "./IMainButton.interface";
 
 const MainButton = ({ text, color, bg, rounded, padding }: IMainButton) => {
     return (
-        <button style={{ minWidth: "15rem", padding: `${padding}`, color: `${color}`, backgroundColor: `${bg}` }}
-            className={`w-100 ${rounded ? "rounded-4" : ""} border-0 text-capitalize`}>
+        <button style={{ minWidth: "13rem", padding: `${padding}`, color: `${color}`, backgroundColor: `${bg}` }}
+            className={`w-75 ${rounded ? "rounded-4" : ""} border-0 text-capitalize`}>
             {text}
         </button>
     );
--- src/pages/Home/HostSolution/HostSolution.tsx
+++ src/pages/Home/HostSolution/HostSolution.tsx
@@ -11,12 +11,12 @@ const HostSolution = () => {
         }}>
 
             <div className="container mt-5">
-                <div className="row g-0">
-                    <div className="col-md-8 text-white" style={{ fontSize: "1.75rem" }}>
+                <div className="row g-4">
+                    <div className="col-md-8 mx-auto text-white" style={{ fontSize: "1.4rem" }}>
                         Dedicated hosting solutions only $129.99/month
                     </div>
 
-                    <div className="col-md-3 d-flex justify-content-start align-items-start mx-auto">
+                    <div className="col-md-4 d-flex justify-content-start align-items-start mx-auto">
                         <MainButton text="get started" rounded={true} bg="var(--pink-color)" color="white" padding="0.91rem" />
                     </div>
                 </div>
