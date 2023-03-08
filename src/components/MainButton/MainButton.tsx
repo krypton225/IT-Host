b5ebe31b916c64517175fb7638caa5f1052103a2
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-08 08:26:26 +0200

    ADD bs-class @ MainButton component

--- src/components/MainButton/MainButton.tsx
+++ src/components/MainButton/MainButton.tsx
@@ -3,7 +3,7 @@ import IMainButton from "./IMainButton.interface";
 const MainButton = ({ text, color, bg, rounded }: IMainButton) => {
     return (
         <button style={{ color: `${color}`, backgroundColor: `${bg}` }}
-            className={`${rounded ? "rounded-4" : ""}`}>
+            className={`${rounded ? "rounded-4" : ""} text-capitalize`}>
             {text}
         </button>
     );
