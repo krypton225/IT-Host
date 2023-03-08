3612d23ec1ccd37a04de5a37987d078f995f0d73
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-08 08:41:27 +0200

    ADD EnterWebsite component @ Home page

---
+++ src/pages/Home/EnterWebsite/EnterWebsite.tsx
@@ -0,0 +1,37 @@
+import { MainButton, MainTitle } from "components";
+import { MainBackground } from "data/photos";
+
+const EnterWebsite = () => {
+    return (
+        <div
+            style={{
+                paddingBlock: "4.5rem",
+                backgroundImage: `url('${MainBackground}')`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+            }}>
+
+            <MainTitle title="enter your website" titleColor="var(--white-color)"
+                description="choose us to keep your business" descriptionColor="#8e8e8e" />
+
+            <div className="container mt-5">
+                <div className="row g-4">
+                    <div className="col-md-8 mx-auto">
+                        <input type="text" name="website-name" id="website-name" style={{ padding: "0.91rem" }}
+                            className="w-100 website-name border-0 rounded-4" placeholder="Enter your website's name" />
+                    </div>
+
+                    <div className="col-md-4 d-flex justify-content-start align-items-start mx-auto">
+                        <MainButton text="search" rounded={true} bg="var(--pink-color)" color="white" padding="0.91rem" />
+                    </div>
+                </div>
+
+                <p className="mt-3 text-white" style={{ fontSize: "0.8rem" }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo corporis beatae nemo!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default EnterWebsite;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,14 @@ import { Features } from "components/";
 
 import { Landing } from "./Landing";
 
+import EnterWebsite from "./EnterWebsite/EnterWebsite";
+
 const Home = () => {
     return (
         <>
             <Landing />
             <Features />
+            <EnterWebsite />
         </>
     );
 };
