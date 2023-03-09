e1f3693aad87d9bdd4e1e0bf8e7d18daf5bb4895
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-09 17:12:10 +0200

    ADD second section @ About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,41 @@
-import { SectionHeader } from "components";
+import { MainButton, SectionHeader } from "components";
 import EnterWebsite from "components/EnterWebsite/EnterWebsite";
+import { AboutPic } from "data/photos";
 
 const About = () => {
     return (
-        <div>
+        <div className="about-us">
             <SectionHeader sectionName="about us" baseURL="home" />
 
-            <EnterWebsite hasBackgroundImage={false} />
+            <EnterWebsite hasBackgroundImage={false} hasDescription={false} />
+
+            <div className="container" style={{ paddingBottom: "6rem", paddingTop: "1.5rem" }}>
+                <div className="row g-5">
+                    <div className="col-lg-6">
+                        <p className="fw-semibold" style={{ fontSize: "1.7rem" }}>
+                            Why choose our company?
+                        </p>
+
+                        <p style={{ marginBlock: "2rem" }}>
+                            Nullam lacinia ex eleifend orci porttitor, suscipit interdum augue condimentum.
+                            Etiam pretium turpis eget nibh laoreet iaculis. Vivamus auctor mi eget odio feugiat.
+                            Quis aliquet velit ornare. Integer egestas sit amet neque sed elementum.
+                            Fusce ut turpis felis. Etiam pretium pharetra augue, ac porttitor dolor consequat eget.
+                            Cras tempor suscipit enim vehicula ultrices.
+                            Mauris sed orci blandit, rhoncus massa non, tincidunt odio. Ut ullamcorper lobortis hendrerit.
+                        </p>
+
+                        <MainButton text="read more" rounded={true} bg="var(--pink-color)"
+                            color="white" padding="0.6rem" halfWidth={true} />
+                    </div>
+
+                    <div className="col-lg-6">
+                        <div className="w-100 h-100">
+                            <img src={AboutPic} alt="About" className="w-100 h-100 cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
