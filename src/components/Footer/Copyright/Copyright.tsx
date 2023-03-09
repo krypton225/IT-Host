c7a2c239a58684e7566cebd0cad82c8a1013a462
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-09 08:37:06 +0200

    ADD Copyright component @ Footer

---
+++ src/components/Footer/Copyright/Copyright.tsx
@@ -0,0 +1,13 @@
+const Copyright = () => {
+    return (
+        <div className="w-100 h-100 px-3 pt-3 d-flex justify-content-center align-items-center text-center"
+            style={{ backgroundColor: "var(--bg-black-rock-color)" }}>
+            <p className="text-white" style={{ fontSize: "0.8rem", color: "var(--text-darker-2)" }}>
+                Copyright &copy;2023 All rights reserved |
+                This template is made with <i className="icon icon-heart-empty"></i> Khaled Mohamed
+            </p>
+        </div>
+    );
+};
+
+export default Copyright;
