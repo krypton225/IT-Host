cf9f8bd0e6e2ba64797e98051e7c4e2035b85f55
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-09 14:26:30 +0200

    ADD starter code template @ pages folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,15 @@
 export { default as Home } from "./Home/Home";
+
+export { default as About } from "./About/About";
+
+export { default as Services } from "./Services/Services";
+
+export { default as News } from "./News/News";
+
+export { default as Contact } from "./Contact/Contact";
+
+export { default as Login } from "./Login/Login";
+
+export { default as Register } from "./Register/Register";
+
+export { default as Error } from "./Error/Error";
