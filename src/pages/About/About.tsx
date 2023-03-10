cab197955a4d1e8b4a9ed8b93025e08efe255b54
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-10 14:01:29 +0200

    ADD ChooseUs component @ About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,10 +4,11 @@ import EnterWebsite from "components/EnterWebsite/EnterWebsite";
 
 import { MainButton, MainTitle, SectionHeader } from "components";
 
-import { AboutPic, MainBackground } from "data/photos";
+import { MainBackground } from "data/photos";
 
 import countingData from "./CountingData/counting-data";
 import teamData from "./Team/team-data";
+import ChooseUs from "./ChooseUs";
 
 const About = () => {
     useEffect(() => {
@@ -20,33 +21,7 @@ const About = () => {
 
             <EnterWebsite hasBackgroundImage={false} hasDescription={false} />
 
-            <div className="container" style={{ paddingBottom: "6rem", paddingTop: "1.5rem" }}>
-                <div className="row g-5">
-                    <div className="col-lg-6">
-                        <p className="fw-semibold" style={{ fontSize: "1.7rem" }}>
-                            Why choose our company?
-                        </p>
-
-                        <p style={{ marginBlock: "2rem" }}>
-                            Nullam lacinia ex eleifend orci porttitor, suscipit interdum augue condimentum.
-                            Etiam pretium turpis eget nibh laoreet iaculis. Vivamus auctor mi eget odio feugiat.
-                            Quis aliquet velit ornare. Integer egestas sit amet neque sed elementum.
-                            Fusce ut turpis felis. Etiam pretium pharetra augue, ac porttitor dolor consequat eget.
-                            Cras tempor suscipit enim vehicula ultrices.
-                            Mauris sed orci blandit, rhoncus massa non, tincidunt odio. Ut ullamcorper lobortis hendrerit.
-                        </p>
-
-                        <MainButton text="read more" rounded={true} bg="var(--pink-color)"
-                            color="white" padding="0.6rem" halfWidth={true} />
-                    </div>
-
-                    <div className="col-lg-6">
-                        <div className="w-100 h-100">
-                            <img src={AboutPic} alt="About" className="w-100 h-100 cover" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ChooseUs />
 
             <div className="counting"
                 style={{
