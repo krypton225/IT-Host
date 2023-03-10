92455dd445690391eecbe4df8965647bc6e25d7b
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-10 21:03:15 +0200

    ADD SectionHeader component @ News page

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,7 +1,14 @@
+import { SectionHeader } from "components";
+import { useEffect } from "react";
+
 const News = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
-        <div>
-            News
+        <div className="news">
+            <SectionHeader sectionName="blog" baseURL="home" />
         </div>
     );
 };
