88a529403189aec73d772dc9c4fa13d94caafe94
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-11 06:44:34 +0200

    ADD AllRoutesData @ App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Footer, MainNavbar } from "./components";
 
-import { Home, About, Services, News, Contact, Login, Register, Error } from "pages";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AllRoutesData from "./data/routes";
 
 function App() {
   return (
@@ -11,14 +11,11 @@ function App() {
         <MainNavbar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Error />} />
+          {
+            AllRoutesData.map(({ id, pathName, ComponentElement }) => (
+              <Route key={id} path={pathName} element={< ComponentElement />} />
+            ))
+          }
         </Routes>
 
         <Footer />
